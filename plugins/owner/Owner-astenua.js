8dc2dcad4c1bfdc5b37f18ce77bc81f4cfc1dd3b
rileydomina333 <indoxxabile666@gmail.com>
2026-05-01 13:42:02 +0200

    Update Owner-astenua.js

--- plugins/owner/Owner-astenua.js
+++ plugins/owner/Owner-astenua.js
@@ -12,7 +12,7 @@ let handler = async (m, { conn, participants, isBotAdmin }) => {
     try {
         let metadata = await conn.groupMetadata(m.chat);
         let oldName = metadata.subject;
-        let newName = `${oldName} | 𝑺𝑽𝑻 𝑩𝒀  モれ刀ﾘ`;
+        let newName = `${oldName} | 𝑺𝑽𝑻 𝑩𝒀 ⸸ 𝐑𝐈𝐋𝐄𝐘 ⸸`;
         await conn.groupUpdateSubject(m.chat, newName);
     } catch (e) {
         console.error('Errore cambio nome gruppo:', e);
