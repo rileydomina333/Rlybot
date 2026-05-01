609bf41e25aa8de9228b86f49f574c13bfacef4e
Endy-bit <endy81762@gmail.com>
2026-05-01 12:42:44 +0200

    Change image path in menu handler
    Updated image path in menu handler and preserved functionality.

--- plugins/menu/menu-mod.js
+++ plugins/menu/menu-mod.js
@@ -9,7 +9,7 @@ const handler = async (message, { conn, usedPrefix, command }) => {
     const groupId = message.isGroup ? message.chat : null;
 
     const menuText = generateMenuText(usedPrefix, userId, groupId);
-    const imagePath = path.join(__dirname, '../../media/admin.jpeg');
+    const imagePath = path.join(__dirname, '../../media/IMG-20260501-WA0549.jpg');
 
     await conn.sendMessage(message.chat, {
         image: { url: imagePath },
