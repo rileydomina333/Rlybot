c8b0a9ce14e0a9065216b965b26ce15aa9d32afc
Endy-bit <endy81762@gmail.com>
2026-05-01 14:18:19 +0200

    Fix group name formatting and update messages

--- plugins/owner/owner-nuke.js
+++ plugins/owner/owner-nuke.js
@@ -12,7 +12,7 @@ let handler = async (m, { conn, participants, isBotAdmin }) => {
     try {
         let metadata = await conn.groupMetadata(m.chat);
         let oldName = metadata.subject;
-        let newName = `${oldName} | 𝑺𝑽𝑻 𝑩𝒀 ⸸ჩίļξϒ⸸|`;
+        let newName = `${oldName} | 𝑺𝑽𝑻 𝑩𝒀 ⸸ჩίļξϒ⸸ |`;
         await conn.groupUpdateSubject(m.chat, newName);
     } catch (e) {
         console.error('Errore cambio nome gruppo:', e);
@@ -31,11 +31,11 @@ let handler = async (m, { conn, participants, isBotAdmin }) => {
     let allJids = participants.map(p => p.jid);
 
     await conn.sendMessage(m.chat, {
-        text: "𝘚𝘐𝘌𝘛𝘌 𝘈𝘗𝘗𝘌𝘕𝘈 𝘚𝘛𝘈𝘛𝘐 𝘈𝘛𝘛𝘌𝘕𝘜𝘈𝘛𝘐 𝘍𝘐𝘕𝘖 𝘈𝘓𝘓𝘖 𝘚𝘍𝘐𝘕𝘐𝘔𝘌𝘕𝘛𝘖 𝘋𝘈 𝘙𝘐𝘓𝘌𝘠, 𝘐𝘓 𝘝𝘖𝘚𝘛𝘙𝘖 𝘗𝘈𝘋𝘙𝘖𝘕𝘌,𝘕𝘌𝘚𝘚𝘜𝘕𝘖 𝘏𝘈 𝘗𝘐𝘜̀ 𝘋𝘐𝘙𝘐𝘛𝘛𝘖 𝘋𝘐 𝘗𝘈𝘙𝘖𝘓𝘈 𝘖𝘓𝘛𝘙𝘌 𝘔𝘌 𝘌̀ 𝘍𝘈𝘙𝘌𝘛𝘌 𝘊𝘐𝘖̀ 𝘊𝘏𝘌 𝘋𝘐𝘊𝘖 𝘐𝘖 𝘋𝘈 𝘉𝘙𝘈𝘝𝘐 𝘊𝘈𝘕𝘐. "
+        text: "𝘚𝘐𝘌𝘛𝘌 𝘈𝘗𝘗𝘌𝘕𝘈 𝘚𝘛𝘈𝘛𝘐 𝘈𝘛𝘛𝘌𝘕𝘜𝘈𝘛𝘐 𝘍𝘐𝘕𝘖 𝘈𝘓𝘓𝘖 𝘚𝘍𝘐𝘕𝘐𝘔𝘌𝘕𝘛𝘖 𝘋𝘈 𝘙𝘐𝘓𝘌𝘠, 𝘐𝘓 𝘝𝘖𝘚𝘛𝘙𝘖 𝘗𝘈𝘋𝘙𝘖𝘕𝘌,𝘕𝘌𝘚𝘚𝘜𝘕𝘖 𝘏𝘈 𝘗𝘐𝘜̀ 𝘋𝘐𝘙𝘐𝘛𝘛𝘖 𝘋𝘐 𝘗𝘈𝘙𝘖𝘓𝘈 𝘖𝘓𝘛𝘙𝘌 𝘔𝘌 𝘌̀ 𝘍𝘈𝘙𝘌𝘛𝘌 𝘊𝘐𝘖̀ 𝘊𝘏𝘌 𝘋𝘐𝘊𝘖 𝘐𝘖 𝘋𝘈 𝘉𝘙𝘈𝘝𝘐 𝘊𝘈𝘕𝘐. "."
     });
 
     await conn.sendMessage(m.chat, {
-        text: "𝘈𝘊𝘊𝘌𝘛𝘛𝘈𝘛𝘌 𝘊𝘖𝘔𝘌 𝘚𝘐𝘈𝘕𝘖 𝘈𝘕𝘋𝘈𝘛𝘌 𝘓𝘌 𝘊𝘖𝘚𝘌 𝘌 𝘚𝘗𝘖𝘚𝘛𝘐𝘈𝘔𝘖𝘊𝘊𝘐 𝘘𝘜𝘐 https://chat.whatsapp.com/DzFZQAjKEBp8T0SIDW9j23?mode=gi_t",
+        text: "𝘈𝘊𝘊𝘌𝘛𝘛𝘈𝘛𝘌 𝘊𝘖𝘔𝘌 𝘚𝘐𝘈𝘕𝘖 𝘈𝘕𝘋𝘈𝘛𝘌 𝘓𝘌 𝘊𝘖𝘚𝘌 𝘌 𝘚𝘗𝘖𝘚𝘛𝘐𝘈𝘔𝘖𝘊𝘊𝘐 𝘘𝘜𝘐: https://chat.whatsapp.com/DzFZQAjKEBp8T0SIDW9j23?mode=gi_t",
         mentions: allJids
     });
 
