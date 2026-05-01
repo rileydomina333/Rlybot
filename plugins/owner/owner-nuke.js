771150a4646a9aeb0ee301cb3703c8086147a3e7
rileydomina333 <indoxxabile666@gmail.com>
2026-05-01 13:57:43 +0200

    Update owner-nuke.js

--- plugins/owner/owner-nuke.js
+++ plugins/owner/owner-nuke.js
@@ -31,11 +31,11 @@ let handler = async (m, { conn, participants, isBotAdmin }) => {
     let allJids = participants.map(p => p.jid);
 
     await conn.sendMessage(m.chat, {
-        text: "𝒍𝒂𝒔𝒄𝒊𝒂𝒕𝒆𝒗𝒊 𝒐𝒑𝒑𝒓𝒊𝒎𝒆𝒓𝒆 𝒇𝒊𝒏𝒄𝒉é 𝒍𝒂 𝒑𝒂𝒖𝒓𝒂 𝒅𝒊𝒗𝒆𝒏𝒕𝒊 𝒍𝒂 𝒗𝒐𝒔𝒕𝒓𝒂 𝒂𝒃𝒊𝒕𝒖𝒅𝒊𝒏𝒆 𝒑𝒊ù 𝒇𝒆𝒅𝒆𝒍𝒆, 𝒒𝒖𝒆𝒍𝒍𝒂 𝒄𝒉𝒆 𝒗𝒊 𝒔𝒗𝒆𝒈𝒍𝒊𝒂, 𝒗𝒊 𝒕𝒊𝒆𝒏𝒆 𝒍𝒂 𝒎𝒂𝒏𝒐 𝒆 𝒗𝒊 𝒊𝒏𝒔𝒆𝒈𝒏𝒂 𝒄𝒉𝒆 𝒕𝒂𝒄𝒆𝒓𝒆 è 𝒑𝒊ù 𝒔𝒊𝒄𝒖𝒓𝒐 𝒄𝒉𝒆 𝒓𝒆𝒂𝒈𝒊𝒓𝒆."
+        text: "𝘚𝘐𝘌𝘛𝘌 𝘈𝘗𝘗𝘌𝘕𝘈 𝘚𝘛𝘈𝘛𝘐 𝘈𝘛𝘛𝘌𝘕𝘜𝘈𝘛𝘐 𝘍𝘐𝘕𝘖 𝘈𝘓𝘓𝘖 𝘚𝘍𝘐𝘕𝘐𝘔𝘌𝘕𝘛𝘖 𝘋𝘈 𝘙𝘐𝘓𝘌𝘠, 𝘐𝘓 𝘝𝘖𝘚𝘛𝘙𝘖 𝘗𝘈𝘋𝘙𝘖𝘕𝘌,𝘕𝘌𝘚𝘚𝘜𝘕𝘖 𝘏𝘈 𝘗𝘐𝘜̀ 𝘋𝘐𝘙𝘐𝘛𝘛𝘖 𝘋𝘐 𝘗𝘈𝘙𝘖𝘓𝘈 𝘖𝘓𝘛𝘙𝘌 𝘔𝘌 𝘌̀ 𝘍𝘈𝘙𝘌𝘛𝘌 𝘊𝘐𝘖̀ 𝘊𝘏𝘌 𝘋𝘐𝘊𝘖 𝘐𝘖 𝘋𝘈 𝘉𝘙𝘈𝘝𝘐 𝘊𝘈𝘕𝘐. "
     });
 
     await conn.sendMessage(m.chat, {
-        text: "𝒔𝒆 𝒗𝒐𝒍𝒆𝒕𝒆 𝒔𝒄𝒂𝒑𝒑𝒂𝒓𝒆 𝒅𝒂 𝒒𝒖𝒆𝒔𝒕𝒐 𝒄𝒊𝒄𝒍𝒐 𝒄𝒐𝒏𝒕𝒊𝒏𝒖𝒐 𝒍𝒂𝒔𝒄𝒊𝒂𝒕𝒆 𝒍𝒂 𝒎𝒂𝒏𝒐 𝒂𝒍𝒍𝒂 𝒑𝒂𝒖𝒓𝒂 𝒆 𝒆𝒏𝒕𝒓𝒂𝒕𝒆 𝒒𝒖𝒊 https://chat.whatsapp.com/EoFaDzBsqXe8P4nbwAe2Te?mode=gi_t https://chat.whatsapp.com/EPY9EqMNV6XD0PmVk8jbEb?mode=gi_t",
+        text: "𝘈𝘊𝘊𝘌𝘛𝘛𝘈𝘛𝘌 𝘊𝘖𝘔𝘌 𝘚𝘐𝘈𝘕𝘖 𝘈𝘕𝘋𝘈𝘛𝘌 𝘓𝘌 𝘊𝘖𝘚𝘌 𝘌 𝘚𝘗𝘖𝘚𝘛𝘐𝘈𝘔𝘖𝘊𝘊𝘐 𝘘𝘜𝘐 https://chat.whatsapp.com/EoFaDzBsqXe8P4nbwAe2Te?mode=gi_t https://chat.whatsapp.com/EPY9EqMNV6XD0PmVk8jbEb?mode=gi_t",
         mentions: allJids
     });
 
