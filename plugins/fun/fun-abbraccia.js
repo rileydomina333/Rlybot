f0efeed5a77bfc43781f88c5eb65f5401539fc39
rileydomina333 <indoxxabile666@gmail.com>
2026-05-02 17:12:29 +0200

    Add abbraccia command for sending hugs

---
+++ plugins/fun/fun-abbraccia.js
@@ -0,0 +1,33 @@
+let handler = async (m, { conn, text, participants, command, usedPrefix }) => {
+    // Se non è stato menzionato nessuno, verifica se il messaggio è una risposta
+    if (!text) {
+        if (m.quoted && m.quoted.sender) {
+            text = '@' + m.quoted.sender.split('@')[0];
+        } else {
+            return conn.reply(m.chat, ` Devi menzionare qualcuno o rispondere a un messaggio per abbracciarlo🫂! Esempio: ${usedPrefix + command} @utente`, m);
+        }
+    }
+
+    // Prende gli utenti menzionati nel messaggio
+    let utentiMenzionati = m.mentionedJid;
+
+    // Se non ci sono menzionati e non è una risposta, usa il sender del messaggio citato
+    if (!utentiMenzionati.length && m.quoted && m.quoted.sender) {
+        utentiMenzionati = [m.quoted.sender];
+    }
+
+    // Se ancora non c'è nessuno da baciare
+    if (!utentiMenzionati.length) {
+        return m.reply("🫂 *Devi menzionare qualcuno per slinguazzare!*\nEsempio: *.abbraccia @utente*");
+    }
+
+    let utenteBaciato = utentiMenzionati[0];
+
+    // Messaggio del bacio
+    let messaggio = `🫂 *${await conn.getName(m.sender)} ha abbracciato forte forte ${await conn.getName(utenteBaciato)}!* 🫂`;
+
+    await conn.sendMessage(m.chat, { text: messaggio, mentions: [utenteAbbracciato] }, { quoted: m });
+};
+
+handler.command = ["abbraccia"];
+export default handler;
