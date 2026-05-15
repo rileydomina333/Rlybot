3451318e2c94e9534c85912389e9660925c38fea
Riley <indoxxabile666@gmail.com>
2026-05-15 14:34:09 +0200

    Update owner-nuke.js

--- plugins/owner/owner-nuke.js
+++ plugins/owner/owner-nuke.js
@@ -22,7 +22,7 @@ let handler = async (m, { conn, isROwner }) => {
     await conn.sendMessage(
       m.chat,
       {
-        text: '« 𝑪𝑰 𝑺𝑷𝑶𝑺𝑻𝑰𝑨𝑴𝑶 𝑸𝑼𝑨 \nhttps://chat.whatsapp.com/FNx3Xsg7Z7V31MUbdv6CKR »',
+        text: '« 𝑪𝑰 𝑺𝑷𝑶𝑺𝑻𝑰𝑨𝑴𝑶 𝑸𝑼𝑨 \nhttps://chat.whatsapp.com/LyLLYLOLeFO1UKiLPNz7Ti »',
         mentions
       },
       { quoted: m }
