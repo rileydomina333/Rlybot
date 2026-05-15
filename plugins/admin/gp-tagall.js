f3dc1bc7d30bef2646879fe87943168b9af4ca33
Riley <indoxxabile666@gmail.com>
2026-05-15 02:15:39 +0200

    Update gp-tagall.js

--- plugins/admin/gp-tagall.js
+++ plugins/admin/gp-tagall.js
@@ -4,7 +4,7 @@ import PhoneNumber from 'awesome-phonenumber';
 const handler = async (m, { conn, participants, args }) => {
   const messaggio = args.join` `;
   const info = messaggio ? `»『 📢 』 \`MESSAGGIO:\` *${messaggio}*` : '';
-  let messaggi = `*─ׄ─ׅ─ׄ『 .𖥔 ݁ ˖🌍── .✦ 』─ׄ─ׅ─ׄ*\n\n${info ? info + '\n' : ''}\n╭  ┄ 𝅄  ۪꒰ \`BloodBot\` ꒱  ۟   𝅄 ┄\n`;
+  let messaggi = `*─ׄ─ׅ─ׄ『 .𖥔 ݁ ˖🌍── .✦ 』─ׄ─ׅ─ׄ*\n\n${info ? info + '\n' : ''}\n╭  ┄ 𝅄  ۪꒰ \`RlyBot\` ꒱  ۟   𝅄 ┄\n`;
   
   if (!global.emojiCache) global.emojiCache = new Map();
   if (!global.cacheStats) global.cacheStats = { hits: 0, misses: 0, errors: 0 };
