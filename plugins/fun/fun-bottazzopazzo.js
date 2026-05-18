d023ddf34d3d3291b95834bafdabba7d13d96b4d
Riley <indoxxabile666@gmail.com>
2026-05-18 15:38:04 +0200

    Create fun-bottazzopazzo.js

---
+++ plugins/fun/fun-bottazzopazzo.js
@@ -0,0 +1,19 @@
+let handler = async (m, { conn }) => {
+
+  const testo = `*🐔CIAO PLEBEO!🦃
+SONO IL 🤖BOTTAZZO PAZZO🤖 FATTO DA RILEY,IL MIO PADRONE. SPAMMA DI COMANDI E TI NUKKO ANCHE IL BUCO DEL CULO COL CAZZO DI RILEY.*`;
+
+  await conn.sendMessage(
+    m.chat,
+    {
+      text: testo
+    },
+    { quoted: m }
+  );
+};
+
+handler.help = ['bottazzo'];
+handler.tags = ['giochi'];
+handler.command = ['bottazzo'];
+
+export default handler;
