530c8cf08e6c9103389917b1b713a46cf1389c1d
Riley <indoxxabile666@gmail.com>
2026-05-19 21:06:42 +0200

    Update fun-rileyplay3.js

--- plugins/fun/fun-rileyplay3.js
+++ plugins/fun/fun-rileyplay3.js
@@ -18,8 +18,8 @@ const handler = async (m, { conn }) => {
     }, { quoted: m })
 }
 
-handler.help = ['rileyplay']
+handler.help = ['rileyplay3']
 handler.tags = ['fun']
-handler.command = /^rileyplay$/i
+handler.command = /^rileyplay3$/i
 
 export default handler
