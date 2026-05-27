0e8f179c84deb04f108d7bc2b8ebb21f2f3fecc2
Riley <indoxxabile666@gmail.com>
2026-05-27 13:25:44 +0200

    Create fun-ciaowinston.js

---
+++ plugins/fun/fun-ciaowinston.js
@@ -0,0 +1,63 @@
+const handler = async (m, { conn, args }) => {
+    if (args.length < 1) {
+        return m.reply(`Uso corretto: .ciaowinston <quantità>\nEsempio: .ciaowinston 5`);
+    }
+
+    let times = parseInt(args[0]);
+    if (isNaN(times) || times < 1) return m.reply("La quantità deve essere un numero valido maggiore di 0.");
+    if (times > 20) times = 20; // Limite per evitare ban/crash
+
+
+    const groupMetadata = await conn.groupMetadata(m.chat);
+    const mentions = groupMetadata.participants.map(u => u.id);
+
+
+    const spamText = 
+`𝐇𝐄𝐘 𝐖𝐈𝐍𝐒𝐓𝐎𝐍:
+CIAO WINSTON
+`;
+
+    const sleep = ms => new Promise(res => setTimeout(res, ms));
+
+    for (let i = 0; i < times; i++) {
+        await conn.relayMessage(
+            m.chat,
+            {
+                requestPaymentMessage: {
+                    noteMessage: {
+                        extendedTextMessage: {
+                            text: spamText,
+                            contextInfo: {
+                                mentionedJid: mentions,
+                                externalAdReply: {
+                                    title: 'AxtralBot Broadcast',
+                                    body: 'Unisciti ora!',
+                                    mediaType: 1,
+                                    renderLargerThumbnail: true,
+                                    showAdAttribution: false
+                                }
+                            }
+                        },
+                        currencyCodeIso4217: 'USD',
+                        requestFrom: '0@s.whatsapp.net',
+                        amount: 99,
+                        expiryTimestamp: Date.now() + 99999
+                    }
+                }
+            },
+            {}
+        );
+
+        if (i < times - 1) {
+    await sleep(800);
+       }
+    }
+};
+
+handler.help = ['ciaowinston <quantità>'];
+handler.tags = ['fun', 'spam'];
+handler.command = /^ciaowinston$/i;
+handler.group = true;
+handler.owner = true;
+
+export default handler;
