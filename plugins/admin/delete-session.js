9e75ad987aefe4513d530476fb9d261486c0f30c
Riley <indoxxabile666@gmail.com>
2026-05-30 04:34:15 +0200

    Update delete-session.js

--- plugins/admin/delete-session.js
+++ plugins/admin/delete-session.js
@@ -6,7 +6,7 @@ const handler = async (message, { conn }) => {
 
   if (global.conn.user.jid !== conn.user.jid) {
     return conn.sendMessage(message.chat, {
-      text: "*🚨 𝐔𝐭𝐢𝐥𝐢𝐳𝐳𝐚 𝐪𝐮𝐞𝐬𝐭𝐨 𝐜𝐨𝐦𝐚𝐧𝐝𝐨 𝐝𝐢𝐫𝐞𝐭𝐭𝐚𝐦𝐞𝐧𝐭𝐞 𝐧𝐞𝐥 𝐧𝐮𝐦𝐞𝐫𝐨 𝐝𝐞𝐥 𝐛𝐨𝐭.*"
+      text: "*💠 𝐔𝐭𝐢𝐥𝐢𝐳𝐳𝐚 𝐪𝐮𝐞𝐬𝐭𝐨 𝐜𝐨𝐦𝐚𝐧𝐝𝐨 𝐝𝐢𝐫𝐞𝐭𝐭𝐚𝐦𝐞𝐧𝐭𝐞 𝐧𝐞𝐥 𝐧𝐮𝐦𝐞𝐫𝐨 𝐝𝐞𝐥 𝐛𝐨𝐭.*"
     }, { quoted: message });
   }
 
@@ -15,7 +15,7 @@ const handler = async (message, { conn }) => {
 
     if (!existsSync(sessionFolder)) {
       return await conn.sendMessage(message.chat, {
-        text: "*❌ 𝐋𝐚 𝐜𝐚𝐫𝐭𝐞𝐥𝐥𝐚 𝐝𝐞𝐥𝐥𝐞 𝐬𝐞𝐬𝐬𝐢𝐨𝐧𝐢 𝐞̀ 𝐯𝐮𝐨𝐭𝐚 𝐨𝐩𝐩𝐮𝐫𝐞 𝐧𝐨𝐧 𝐞𝐬𝐢𝐬𝐭𝐞.*"
+        text: "*💠 𝐋𝐚 𝐜𝐚𝐫𝐭𝐞𝐥𝐥𝐚 𝐝𝐞𝐥𝐥𝐞 𝐬𝐞𝐬𝐬𝐢𝐨𝐧𝐢 𝐞̀ 𝐯𝐮𝐨𝐭𝐚 𝐨𝐩𝐩𝐮𝐫𝐞 𝐧𝐨𝐧 𝐞𝐬𝐢𝐬𝐭𝐞.*"
       }, { quoted: message });
     }
 
@@ -30,14 +30,14 @@ const handler = async (message, { conn }) => {
     }
 
     const responseText = deletedCount === 0
-      ? "❗ 𝐋𝐞 𝐬𝐞𝐬𝐬𝐢𝐨𝐧𝐢 𝐬𝐨𝐧𝐨 𝐯𝐮𝐨𝐭𝐞"
-      : `*🔥 𝐒𝐨𝐧𝐨 𝐬𝐭𝐚𝐭𝐢 𝐞𝐥𝐢𝐦𝐢𝐦𝐚𝐧𝐢 ${deletedCount} 𝐚𝐫𝐜𝐡𝐢𝐯𝐢 𝐝𝐚𝐥𝐥𝐞 𝐬𝐞𝐬𝐬𝐢𝐨𝐧𝐢, 𝐆𝐫𝐚𝐳𝐢𝐞 𝐩𝐞𝐫 𝐚𝐯𝐞𝐫𝐦𝐢 𝐬𝐯𝐮𝐨𝐭𝐚𝐭𝐨!*`;
+      ? "💠 𝐋𝐞 𝐬𝐞𝐬𝐬𝐢𝐨𝐧𝐢 𝐬𝐨𝐧𝐨 𝐯𝐮𝐨𝐭𝐞"
+      : `*💠 𝐒𝐨𝐧𝐨 𝐬𝐭𝐚𝐭𝐢 𝐞𝐥𝐢𝐦𝐢𝐦𝐚𝐧𝐢 ${deletedCount} 𝐚𝐫𝐜𝐡𝐢𝐯𝐢 𝐝𝐚𝐥𝐥𝐞 𝐬𝐞𝐬𝐬𝐢𝐨𝐧𝐢, 𝐆𝐫𝐚𝐳𝐢𝐞 𝐩𝐞𝐫 𝐚𝐯𝐞𝐫𝐦𝐢 𝐬𝐯𝐮𝐨𝐭𝐚𝐭𝐨!*`;
 
-    // 🔘 Bottoni aggiunti
+    // 💠 Bottoni aggiunti
     const buttons = [
-      { buttonId: "ds", buttonText: { displayText: "🔄 𝐒𝐯𝐮𝐨𝐭𝐚 𝐝𝐢 𝐧𝐮𝐨𝐯𝐨" }, type: 1 },
-      { buttonId: "ping", buttonText: { displayText: "📊 𝐏𝐢𝐧𝐠" }, type: 1 },
-      { buttonId: "pong", buttonText: { displayText: "⚡ 𝐏𝐨𝐧𝐠" }, type: 1 }
+      { buttonId: "ds", buttonText: { displayText: "💠 𝐒𝐯𝐮𝐨𝐭𝐚 𝐝𝐢 𝐧𝐮𝐨𝐯𝐨" }, type: 1 },
+      { buttonId: "ping", buttonText: { displayText: "💠 𝐏𝐢𝐧𝐠" }, type: 1 }
+      
     ];
 
     await conn.sendMessage(
@@ -52,7 +52,7 @@ const handler = async (message, { conn }) => {
 
   } catch (error) {
     console.error('Errore:', error);
-    await conn.sendMessage(message.chat, { text: "❌ Errore di eliminazione!" }, { quoted: message });
+    await conn.sendMessage(message.chat, { text: "💠 Errore di eliminazione!" }, { quoted: message });
   }
 };
 
