4023efa30c385e914bd85ee18fc1ec0eb5f75e12
Riley <indoxxabile666@gmail.com>
2026-05-30 18:08:06 +0200

    Update gp-setname.js

--- plugins/admin/gp-setname.js
+++ plugins/admin/gp-setname.js
@@ -1,16 +1,16 @@
 let handler = async (m, { conn, text, isBotAdmin, isAdmin }) => {
-  if (!m.isGroup) return m.reply('❌ Questo comando funziona solo nei gruppi.')
-  if (!isBotAdmin) return m.reply('❌ Devo essere admin per cambiare il nome.')
-  if (!isAdmin) return m.reply('❌ Solo gli admin possono usare questo comando.')
-  if (!text) return m.reply('✏️ Usa così:\n.setname Nuovo Nome Gruppo')
+  if (!m.isGroup) return m.reply('💠 Questo comando funziona solo nei gruppi.')
+  if (!isBotAdmin) return m.reply('💠 Devo essere admin per cambiare il nome.')
+  if (!isAdmin) return m.reply('💠 Solo gli admin possono usare questo comando.')
+  if (!text) return m.reply('💠 Usa così:\n.setname Nuovo Nome Gruppo')
 
   try {
     await conn.groupUpdateSubject(m.chat, text)
 
-    await m.reply(`✅ Nome del gruppo cambiato in:\n*${text}*`)
+    await m.reply(`💠 Nome del gruppo cambiato in:\n*${text}*💠`)
   } catch (e) {
     console.error('Errore setname:', e)
-    m.reply('⚠️ Errore durante il cambio nome.')
+    m.reply('💠 Errore durante il cambio nome.💠')
   }
 }
 
