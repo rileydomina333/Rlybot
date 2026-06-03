f32d6abd06e6727b72f3e6c95e296f8a6daff03e
Riley <indoxxabile666@gmail.com>
2026-06-04 00:24:19 +0200

    Update fun-domanda.js

--- plugins/fun/fun-domanda.js
+++ plugins/fun/fun-domanda.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn }) => {
 
-  const testo = `*🤖:io penso che devi abusarli e farli piangere tutti,altrimenti ti rolli mezza canna e fumi come un jamaicani in pensione proprio come bob marley.*`;
+  const testo = `*🤖:io penso che devi abusarli e farli piangere tutti,altrimenti ti rolli mezza canna e fumi come un jamaicano in pensione proprio come bob marley.*`;
 
   await conn.sendMessage(
     m.chat,
