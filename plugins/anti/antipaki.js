61e4ac126e741c83c567d4c9d497ddd581e60625
𝑬𝒍𝒊𝒙𝒊𝒓 <165661846+Elixir-png@users.noreply.github.com>
2026-06-04 04:38:49 +0200

    Update antipaki.js

--- plugins/anti/antipaki.js
+++ plugins/anti/antipaki.js
@@ -1,12 +1,15 @@
-@@ -3,6 +3,10 @@
 let handler = m => m
 handler.before = async function (m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) {
     if (!m.isGroup) return !1
     let chat = global.db.data.chats[m.chat]
     let bot = global.db.data.settings[conn.user.jid] || {}
 
-@@ -13,4 +17,4 @@ handler.before = async function (m, {conn, isAdmin, isBotAdmin, isOwner, isROwne
+    if (chat?.antipaki && !isAdmin && !isOwner && !isROwner) {
+        if (m.text && /pakistan|paki|pakistano/i.test(m.text)) {
+            if (isBotAdmin) {
+                await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove').catch(() => {})
+            }
         }
     }
 }
-export default handler;
+export default handler
