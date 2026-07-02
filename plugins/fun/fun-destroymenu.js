e805dc86a4986797c78a43c086d672b6984d0e42
Riley <indoxxabile666@gmail.com>
2026-07-02 16:36:40 +0200

    Update fun-destroymenu.js

--- plugins/fun/fun-destroymenu.js
+++ plugins/fun/fun-destroymenu.js
@@ -1,6 +1,11 @@
 let handler = async (m, { conn }) => {
 
-  const testo = `*𝗡𝗨𝗞𝗘 𝗩𝟭: .𝗙𝗔𝗧𝗔𝗟𝗜𝗧𝗬 𝗡𝗨𝗞𝗘 𝗩𝟮: .𝗕𝗥𝗨𝗧𝗔𝗟𝗜𝗧𝗬 𝗡𝗨𝗞𝗘 𝗩𝟯: .𝗗𝗜𝗦𝗦𝗢𝗖𝗜𝗔𝗧𝗢 𝗦𝗣𝗔𝗠 𝗩𝟭: .𝗪𝗔𝗞𝗘𝗨𝗣𝗡𝗢𝗪 𝗦𝗣𝗔𝗠 𝗩𝟮: . 𝗚𝗛𝗨𝗚𝗛𝗨𝗚𝗛𝗔𝗚𝗛𝗔 𝗦𝗣𝗔𝗠 𝗩𝟯: .𝗙𝗕𝗜𝗢𝗣𝗘𝗡𝗨𝗣*`;
+  const testo = `*NUKE V1: .FATALITY
+NUKE V2: .BRUTALITY
+NUKE V3: .DISSOCIATO
+SPAM V1: .WAKEUPNOW
+SPAM V2: .GHUGHUGHAGHA
+SPAM V3: .FBIOPENUP*`;
 
   await conn.sendMessage(
     m.chat,
