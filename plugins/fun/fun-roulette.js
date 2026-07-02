6b5c28cf9864201fc9e3cad8e8a9d98e47ca31a7
Riley <indoxxabile666@gmail.com>
2026-07-02 18:57:28 +0200

    Update fun-roulette.js

--- plugins/fun/fun-roulette.js
+++ plugins/fun/fun-roulette.js
@@ -25,7 +25,7 @@ let handler = async (m, { conn, text, command, usedPrefix }) => {
         { buttonId: `${usedPrefix}${command} 25 red`, buttonText: { displayText: global.t('rouletteButtonRed25', userId, groupId) }, type: 1 },
         { buttonId: `${usedPrefix}${command} 50 red`, buttonText: { displayText: global.t('rouletteButtonRed50', userId, groupId) }, type: 1 }
       ],
-      footer: '🎰 ChatUnity Roulette'
+      footer: '🎰 rlybot Roulette'
     }, { quoted: m });
   }
 
