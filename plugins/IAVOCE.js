51a8a109d5ebb345372e661c45b675e84ee29c8d
Riley <indoxxabile666@gmail.com>
2026-07-03 17:35:11 +0200

    Update IAVOCE.js

--- plugins/IAVOCE.js
+++ plugins/IAVOCE.js
@@ -1,5 +1,6 @@
 import { exec } from 'child_process'
 import fs from 'fs'
+import path from 'path'
 import { randomUUID } from 'crypto'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
@@ -8,20 +9,35 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 
     await conn.sendPresenceUpdate('recording', m.chat)
 
-    const fileName = `./tmp/${randomUUID()}.mp3`
-    const textPulito = text.replace(/"/g, '\\"') // evita command injection
+    // 1. Crea cartella tmp se non esiste
+    const tmpDir = './tmp'
+    if (!fs.existsSync(tmpDir)) fs.mkdirSync(tmpDir)
+
+    const fileName = path.join(tmpDir, `${randomUUID()}.mp3`)
+    const textPulito = text.replace(/"/g, '\\"').replace(/`/g, '\\`')
 
     try {
-        // 1. Genera audio con espeak-ng in italiano
-        // -v it = voce italiana, -s 150 = velocità, -p 50 = pitch
+        // 2. Controlla se espeak-ng esiste
         await new Promise((resolve, reject) => {
-            exec(`espeak-ng -v it -s 150 -p 50 "${textPulito}" --stdout | ffmpeg -i - -ar 24000 -ac 2 -ab 48k -f mp3 ${fileName}`, (err) => {
-                if (err) reject(err)
+            exec('which espeak-ng', (err) => {
+                if (err) reject('ESPEAK_MANCANTE')
                 else resolve()
             })
         })
 
-        // 2. Leggi e invia
+        // 3. Genera audio
+        await new Promise((resolve, reject) => {
+            exec(`espeak-ng -v it -s 150 -p 50 "${textPulito}" --stdout | ffmpeg -i - -ar 24000 -c:a libmp3lame -b:a 48k -f mp3 ${fileName}`, (err, stdout, stderr) => {
+                if (err) {
+                    console.log('Errore espeak/ffmpeg:', stderr)
+                    reject('ERRORE_GENERAZIONE')
+                } else resolve()
+            })
+        })
+
+        // 4. Controlla se il file è stato creato
+        if (!fs.existsSync(fileName)) throw 'FILE_NON_CREATO'
+
         let audioBuffer = fs.readFileSync(fileName)
         await conn.sendMessage(m.chat, { 
             audio: audioBuffer, 
@@ -31,15 +47,21 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 
     } catch (e) {
         console.log(e)
-        m.reply('Errore: espeak-ng non è installato sul server.')
+        if (e === 'ESPEAK_MANCANTE') {
+            m.reply('❌ Errore: espeak-ng non installato.\nEsegui sul server:\n`sudo apt install espeak-ng ffmpeg -y`')
+        } else if (e === 'ERRORE_GENERAZIONE') {
+            m.reply('❌ Errore: ffmpeg non installato.\nEsegui sul server:\n`sudo apt install ffmpeg -y`')
+        } else {
+            m.reply('❌ Errore nella generazione audio. Controlla i log.')
+        }
     } finally {
-        // 3. Pulisci il file temporaneo
+        // 5. Pulisci sempre il file
         if (fs.existsSync(fileName)) fs.unlinkSync(fileName)
     }
 }
 
-handler.command = /^vocalbot$/i
+handler.command = /^audio$/i
 handler.tags = ['tools']
-handler.help = ['vocalbot <testo>']
+handler.help = ['audio <testo>']
 
 export default handler
