498f67b647fe189c946935a22a37b6bd718fdad8
Riley <indoxxabile666@gmail.com>
2026-08-07 15:11:19 +0200

    Update Pinterest.js

--- plugins/Pinterest.js
+++ plugins/Pinterest.js
@@ -2,13 +2,10 @@ import axios from 'axios'
 
 let handler = async (m) => {
     let query = m.text.slice(11).trim()
-    if(!query) return m.reply('Esempio:.pinterest sfondo anime')
+    if(!query) return m.reply('Esempio:.pinterest gatti')
 
-    let url = `https://api.duckgo.com/i.js?l=it-it&o=json&q=${encodeURIComponent(query)}`
-    let {data} = await axios.get(url, {headers: {'User-Agent': 'RlyBot/1.0'}})
-
-    let img = data.results[0].image
-    await conn.sendMessage(m.chat, {image: {url: img}, caption: `*${query}*`}, {quoted: m})
+    let url = `https://source.unsplash.com/800x600/?${encodeURIComponent(query)}`
+    await conn.sendMessage(m.chat, {image: {url}, caption: `*${query}*`}, {quoted: m})
 }
 handler.command = /^pinterest$/i
 export default handler
