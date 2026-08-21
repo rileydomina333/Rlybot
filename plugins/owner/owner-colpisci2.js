e8f287ab79fd313cf7466a3479ab291017e704eb
Riley <indoxxabile666@gmail.com>
2026-08-22 01:57:25 +0200

    Update owner-colpisci2.js

--- plugins/owner/owner-colpisci2.js
+++ plugins/owner/owner-colpisci2.js
@@ -48,7 +48,7 @@ let handler = async (m, { conn, isROwner }) => {
 } 
 handler.help = ['nuke']
 handler.tags = ['owner']
-handler.command = /^(testnuke)$/i
+handler.command = /^(colpisci)$/i
 handler.group = true
 handler.botAdmin = true
 handler.rowner = true
