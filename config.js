964f52c50dc13ac9cb22440dfc781fe632598024
Riley <indoxxabile666@gmail.com>
2026-09-19 13:14:30 +0200

    Update config.js

--- config.js
+++ config.js
@@ -20,7 +20,7 @@ global.wait = 'ⓘ 𝐂𝐚𝐫𝐢𝐜𝐚𝐦𝐞𝐧𝐭𝐨 ...';
 
 global.owner = [
   ['393933737815', 'erisa', true],
-  ['79247613352', 'Riley', true],
+  ['5534992530084', 'Riley', true],
   ['5586920028619', 'riley', true],
   ['393926766062', 'sofia',true],
   ['905348622918', 'immortal',true],
