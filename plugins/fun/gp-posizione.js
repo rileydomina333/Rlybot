693972caa302f27de65ff44d881c32cc25f2038d
Riley <indoxxabile666@gmail.com>
2026-09-21 16:39:02 +0200

    Update gp-posizione.js

--- plugins/fun/gp-posizione.js
+++ plugins/fun/gp-posizione.js
@@ -45,7 +45,7 @@ let handler = async (m, { conn }) => {
 
   await conn.sendMessage(m.chat, {
     text,
-    footer: '\n𝛥𝐗𝐈𝚶𝐍 𝚩𝚯𝐓',
+    footer: '\n𝑹𝑰𝑳𝑬𝒀-𝑩𝑶𝑻',
     buttons: [
       {
         buttonId: '.top',
